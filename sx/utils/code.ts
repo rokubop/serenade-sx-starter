23ee97f224cdc2178af8ab128fdc5af1570d161a
Bradley Neumaier <neumaierbradley@icloud.com>
2023-07-09 20:03:12 -0400

    Modify VS Code logic to work on macOS

--- sx/utils/code.ts
+++ sx/utils/code.ts
@@ -1,4 +1,4 @@
-import { execa } from "execa";
+import { execa, ExecaError } from "execa";
 import { isLinux, isMac, isWindows } from "./utils";
 import browser from "sx/browser";
 
@@ -13,28 +13,46 @@ export async function isVSCodeInPath() {
 }
 
 export async function openPathInVSCode(path: string) {
-  const hasCode = await isVSCodeInPath();
-
-  if (!hasCode) {
-    await browser.displayWarningHtml(`
-        <h1>Could not find VS Code in PATH</h1>
-        <div>
-            <p>In VSCode, open the command palette (CMD+Shift+P on Mac and CTRL+Shift+P on Windows) and run <code>Install 'code' command in PATH</code></p>
-        </div>
-    `);
-    return;
-  }
-
   if (isWindows()) {
-    await execa("code", [path]);
+    await tryToLaunchCode("code", [path]);
   } else if (isMac()) {
-    await execa("open", ["-a", "Visual Studio Code", path]);
+    await tryToLaunchCode("open", ["-a", "Visual Studio Code", path]);
   } else if (isLinux()) {
-    await execa("code", [path]);
+    await tryToLaunchCode("code", [path]);
   } else {
     await browser;
     throw new Error(
       "Opening file in VS Code for OS not implemented. Do you have 'code' command installed?"
     );
+  }
+}
+async function tryToLaunchCode(
+  commandName: string,
+  args: string[]
+): Promise<void> {
+  try {
+    await execa(commandName, args);
+  } catch (error) {
+    const execaError = error as ExecaError;
+    const hasCode = await isVSCodeInPath();
+
+    if (!hasCode) {
+      await browser.displayWarningHtml(`
+        <h1>Could not find VS Code in PATH</h1>
+        <div>
+            <p>In VSCode, open the command palette (CMD+Shift+P on Mac and CTRL+Shift+P on Windows) and run <code>Install 'code' command in PATH</code></p>
+            <p>Error message: ${execaError.message}</p>
+            <p>Error code: ${execaError.exitCode}</p>
+        </div>
+    `);
+    } else {
+      await browser.displayWarningHtml(`
+        <h1>Failed to open VS Code</h1>
+        <div>
+            <p>Error message: ${execaError.message}</p>
+            <p>Error code: ${execaError.exitCode}</p>
+        </div>
+    `);
+    }
   }
 }
